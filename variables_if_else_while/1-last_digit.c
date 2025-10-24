500bde76e21ff2ed712392a4f5d6c750768cd7a3
Sajed <12205@holbertonstudents.com>
2025-10-24 10:05:52 +0200

    Fix Betty style for 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,32 @@
-if (last_digit > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-else if (last_digit == 0)
-	printf("Last digit of %d is %d and is 0\n", n, last_digit);
-else
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints whether the last digit of n is positive, zero, or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	}
+
+	return (0);
+}
