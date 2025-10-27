27d4e840616bb28d6230f7f1f430a7ade25e0365
Sajed <12205@holbertonstudents.com>
2025-10-27 20:45:31 +0100

    Fix Betty style in 4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: The character to check
+ * @c: the character to check
  *
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 int _isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return (1);
-    else
-        return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
