2172ea576998f69b38cb088f260e728eb15e33a4
Sajed <12205@holbertonstudents.com>
2025-10-29 19:54:08 +0100

    Recreate 0-reset_to_98.c to reset an integer to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of the integer pointed to by n to 98
- * @n: pointer to an integer
+ * reset_to_98 - updates the value it points to 98
+ * @n: pointer to an int
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
