61624b2002a70279d997820a9670ba34e34caf3d
Sajed <12205@holbertonstudents.com>
2025-10-30 22:07:30 +0100

    Add 10-add function

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
+int add(int a, int b);
 
-#endif
+#endif /* MAIN_H */
