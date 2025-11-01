a56a4dbe90f7b506b2868d1d8bf032dbbddf15ed
Sajed <12205@holbertonstudents.com>
2025-11-01 21:29:23 +0100

    Add _strlen function with documentation

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,14 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
 int _strlen(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
 }
