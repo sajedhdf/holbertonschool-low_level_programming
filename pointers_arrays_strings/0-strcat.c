d2f3e4ded0ed2617e9324b4e6d61de35da5e41a2
Sajed <12205@holbertonstudents.com>
2025-11-05 16:50:43 +0100

    Add 0-strcat.c function to concatenate two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0' ; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
