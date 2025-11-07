ad35cdacc47e5065c8b92af07115fa50595c03f6
Sajed <12205@holbertonstudents.com>
2025-11-07 23:41:41 +0100

    Add implementation of _memcpy function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void reset_to_98(int *n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
