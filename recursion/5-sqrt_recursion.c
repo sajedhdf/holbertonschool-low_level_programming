343b590f62863b2d237ded8cb0369d9a16b8a24f
Sajed <12205@holbertonstudents.com>
2025-12-02 20:01:01 +0100

    Fix Betty style for 5-sqrt_recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,19 +1,32 @@
 #include "main.h"
 
+/**
+ * _sqrt_helper - helper function to find natural square root
+ * @n: number to find the square root of
+ * @guess: current number being tested
+ *
+ * Return: natural square root, or -1 if none found
+ */
 int _sqrt_helper(int n, int guess)
 {
-    if (guess * guess == n)
-        return (guess);
-    if (guess * guess > n)
-        return (-1);
+	if ((guess * guess) == n)
+		return (guess);
+	if ((guess * guess) > n)
+		return (-1);
 
-    return _sqrt_helper(n, guess + 1);
+	return (_sqrt_helper(n, guess + 1));
 }
 
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: natural square root, or -1 if none exists
+ */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
+	if (n < 0)
+		return (-1);
 
-    return _sqrt_helper(n, 0);
+	return (_sqrt_helper(n, 0));
 }
