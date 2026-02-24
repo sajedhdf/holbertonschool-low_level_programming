1b614af0a1846f54a404f53bb6f92aa7b937ac40
sajedhdf <12205@holbertonstudents.com>
2026-02-24 20:10:18 +0000

    update the documentation betty

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
-/*
- * main - prints the lowercase alphabet followed by a newline
- *
- * Return: Always 0 (Success)
- *
 /**
  * main - prints the lowercase alphabet followed by a newline
  *
  * Return: Always 0 (Success)
- *//
+ */
 int main(void)
 {
 	char c;
