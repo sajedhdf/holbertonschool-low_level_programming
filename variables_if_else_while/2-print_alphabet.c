91e1ba89cf4f4f822ebcf364c9aea03655985076
sajedhdf <12205@holbertonstudents.com>
2026-02-24 20:07:00 +0000

    add a new file

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /*
-This program prints the lowercase alphabet followed by a newline.
-It uses only two putchar calls.
-All code is inside the main function.
-*/
-
+ * main - prints the lowercase alphabet followed by a newline
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-        putchar(c);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
