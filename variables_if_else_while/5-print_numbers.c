b08c2e3bc4b7172326f914f027dc74a87a9942a7
sajedhdf <12205@holbertonstudents.com>
2026-02-25 08:57:17 +0000

    add a new file

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stio.h>
+
+/**
+ * main - prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	if (x < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
