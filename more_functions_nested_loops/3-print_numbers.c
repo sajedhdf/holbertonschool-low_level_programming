2d102f16bfbfa74d4b000cc82a833ccc6a840857
sajedhdf <12205@holbertonstudents.com>
2026-03-02 14:02:37 +0000

    add a new file with putchar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* print_numbers - prints the numbers from 0 to 9
+*
+* Description: Prints each digit on the same line,
+* followed by a new line.
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar('0' + i);
+
+	putchar('\n');
+}
