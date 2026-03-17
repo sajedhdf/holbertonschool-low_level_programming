8c17dd67ea692e71077b98c021ea78f4ea1ed961
sajedhdf <12205@holbertonstudents.com>
2026-03-17 21:42:33 +0000

    capitalizes all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * cap_string - capitalizes all words of a string
  * @s: string to modify
@@ -7,15 +9,26 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	char sep[] = " \t\n,;.!?\"(){}";
+	int j;
 
-	while (s[i])
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+
+	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		j = 0;
+		while (sep[j] != '\0')
 		{
-			if (i == 0 || s[i - 1] == ' ')
-				s[i] -= 32;
+			if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+				break;
+			}
+			j++;
 		}
 		i++;
 	}
+
 	return (s);
 }
