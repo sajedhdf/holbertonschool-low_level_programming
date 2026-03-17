1729162f5614e0199003ece52a597ae66ceaca6b
sajedhdf <12205@holbertonstudents.com>
2026-03-17 21:20:50 +0000

    add functions to reverse an array and convert string to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @str: string to modify
+ *
+ * Return: pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+
+		i++;
+	}
+
+	return (str);
+}
