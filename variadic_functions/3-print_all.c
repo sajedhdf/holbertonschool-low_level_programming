6ba63d13cef65df5fc14bb7e4b7752844ea5294c
sajedhdf <12205@holbertonstudents.com>
2026-03-30 19:42:39 +0000

    function that prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -14,10 +14,13 @@ void print_all(const char * const format, ...)
 	va_start(ap, format);
 	while (format && format[i])
 	{
-		if (format[i] == 'c') printf("%s%c", sep, va_arg(ap, int)), sep = ", ";
-		if (format[i] == 'i') printf("%s%d", sep, va_arg(ap, int)), sep = ", ";
-		if (format[i] == 'f') printf("%s%f", sep, va_arg(ap, double)), sep = ", ";
-		if (format[i] == 's') s = va_arg(ap, char *), printf("%s%s", sep, s ? s : "(nil)"), sep = ", ";
+		if (format[i] == 'c' || format[i] == 'i')
+			printf(format[i] == 'c' ? "%s%c" : "%s%d", sep, va_arg(ap, int)), sep = ", ";
+		if (format[i] == 'f' || format[i] == 's')
+			format[i] == 'f'
+			? printf("%s%f", sep, va_arg(ap, double))
+			: (s = va_arg(ap, char *), printf("%s%s", sep, s ? s : "(nil)")),
+			sep = ", ";
 		i++;
 	}
 	printf("\n");
