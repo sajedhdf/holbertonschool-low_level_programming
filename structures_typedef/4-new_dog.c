19e72295f5134d08b58f59c13c6a6fca670636ad
Sajed <12205@holbertonstudents.com>
2026-04-15 22:48:59 +0200

    creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,48 +1,87 @@
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * new_dog - creates a new dog
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
+ * _strlen - returns the length of a string
+ * @s: string
  *
- * Return: pointer to new dog, or NULL on failure
+ * Return: length of the string
  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ *new_dog - creates a new dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *
+ *Return: pointer to new dog, or NULL if it fails
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_d;
+	dog_t *dog;
 	char *name_copy;
 	char *owner_copy;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	new_d = malloc(sizeof(dog_t));
-	if (new_d == NULL)
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
 
-	name_copy = malloc(strlen(name) + 1);
+	name_copy = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (name_copy == NULL)
 	{
-		free(new_d);
+		free(dog);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
 
-	owner_copy = malloc(strlen(owner) + 1);
+	owner_copy = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
-		free(new_d);
+		free(dog);
 		return (NULL);
 	}
-	strcpy(owner_copy, owner);
 
-	new_d->name = name_copy;
-	new_d->age = age;
-	new_d->owner = owner_copy;
+	_strcpy(name_copy, name);
+	_strcpy(owner_copy, owner);
 
-	return (new_d);
+	dog->name = name_copy;
+	dog->age = age;
+	dog->owner = owner_copy;
+
+	return (dog);
 }
