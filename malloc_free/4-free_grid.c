bb3fbe8d698a8112e96507c71b27a07cda123931
Sajed <12205@holbertonstudents.com>
2026-04-26 00:49:58 +0200

    add a doc betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * free_grid - frees a 2 dimensional grid previously created
+ * by alloc_grid
+ * @grid: pointer to the grid
+ * @height: number of rows of the grid
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
